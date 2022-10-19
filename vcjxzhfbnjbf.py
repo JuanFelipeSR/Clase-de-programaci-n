6bafb3e1f0d7d27364c795ed62610a8c81eca86c
CSU21-AINF <CSU21@academico.unab.ad>
2022-10-19 17:31:36 -0500

    Miprimer proyecto en python

---
+++ vcjxzhfbnjbf.py
@@ -0,0 +1,10 @@
+class basketballplayer:
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+    
+   
+
+p1=basketballplayer("Lebron", "23")
+print(p1.name)
+print(p1.number)
